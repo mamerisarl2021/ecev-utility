78d48f34e3149391d364080bb75413c686c2ebb7
Djamal GANI <yowedjamal@gmail.com>
2025-03-28 17:31:30 +0100

    Mise à jour du nginx

--- app/settings.py
+++ app/settings.py
@@ -181,6 +181,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/Pdfs/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'PDFs')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_ROOT = '/PDFs/'
