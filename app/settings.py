498aa6f0f24e7d204f49fda527843894e1e973c4
Djamal GANI <yowedjamal@gmail.com>
2025-03-28 17:06:58 +0100

    Mise à jour du conf nginx

--- app/settings.py
+++ app/settings.py
@@ -17,7 +17,6 @@ import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-sys.path.insert(0, str(BASE_DIR.parent))  # Adds /app to sys.path
 
 
 # Quick-start development settings - unsuitable for production
